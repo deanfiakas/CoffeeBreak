e57f542afbd102f68fbcafb8045a1d22f9d0ae2e
Konstandinos Fiakas <Konstandinos.fiakas.2015@uni.strath.ac.uk>
2017-12-01 23:03:51 +0000

    LocationTransmitter is now a service. Ready to push into Firebase.

--- app/src/main/java/com/example/dean/coffeebreak/Workers/LocationTransmitter.java
+++ app/src/main/java/com/example/dean/coffeebreak/Workers/LocationTransmitter.java
@@ -1,5 +1,15 @@
 package com.example.dean.coffeebreak.Workers;
 
+import android.app.IntentService;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Worker class than transmits coordinates to the back end on fixed intervals.
  * Created upon login. Terminates upon logging out or killing the app.
@@ -7,11 +17,35 @@ package com.example.dean.coffeebreak.Workers;
  * Should go to sleep on app close and back up running on resume
  */
 
-public class LocationTransmitter implements Runnable {
+public class LocationTransmitter extends IntentService{
+
+    public LocationTransmitter(String name) {
+        super(name);
+    }
 
     @Override
-    public void run() {
-        //Get lat, lon -> send them to firebase every 10 seconds
+    protected void onHandleIntent(@Nullable Intent intent) {
+        // Acquire a reference to the system Location Manager
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        // Define a listener that responds to location updates
+        LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                // Called when a new location is found by the network location provider.
+                // CALL FIREBASE YOH
+            }
 
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+            public void onProviderEnabled(String provider) {}
+
+            public void onProviderDisabled(String provider) {}
+        };
+
+        // Register the listener with the Location Manager to receive location updates
+        try {
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+        } catch (SecurityException ex){
+
+        }
     }
 }
