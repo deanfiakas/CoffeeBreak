529a0ee6c66bab89f6d04cc4cf7db28be763d4cf
Konstandinos Fiakas <Konstandinos.fiakas.2015@uni.strath.ac.uk>
2017-12-01 22:45:08 +0000

    More stuff

--- app/src/main/java/com/example/dean/coffeebreak/Views/MainActivity.java
+++ app/src/main/java/com/example/dean/coffeebreak/Views/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.dean.coffeebreak.Views;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-
 import com.example.dean.coffeebreak.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity  implements OnMapReadyCallback {
+    final int MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION = 500;
+    private int granted = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +23,51 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        MapFragment mapFragment = (MapFragment)getFragmentManager().findFragmentById(R.id.mapFragment);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        // TODO: Perhaps show markers with locations? Permissions should be moved to WelcomeActivity.
+        try {
+            requestPermission();
+            googleMap.setMyLocationEnabled(true);
+        } catch (SecurityException ex){
+            ex.printStackTrace();
+        }
+    }
+
+
+    public void requestPermission(){
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_CONTACTS)) {
+                    // Show user explanation
+            } else {
+                  ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION);
+            }
+        }
 
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    granted = 1;
+                }
+                return;
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/dean/coffeebreak/Workers/LocationTransmitter.java
+++ app/src/main/java/com/example/dean/coffeebreak/Workers/LocationTransmitter.java
@@ -3,12 +3,15 @@ package com.example.dean.coffeebreak.Workers;
 /**
  * Worker class than transmits coordinates to the back end on fixed intervals.
  * Created upon login. Terminates upon logging out or killing the app.
+ *
+ * Should go to sleep on app close and back up running on resume
  */
 
 public class LocationTransmitter implements Runnable {
 
     @Override
     public void run() {
+        //Get lat, lon -> send them to firebase every 10 seconds
 
     }
 }
